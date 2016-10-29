3e9dafc0dba802965777b072166d8529b8ecb4e7
KulbachPolina <Polinalexia@gmail.com>
2016-10-29 20:37:41 +0300

    my commit

---
+++ Vinzenere.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main (int argc, string argv[])
+{
+    if(argc !=2)
+    {
+        printf("You have missed a key or entered more than one");
+        return 1;
+    }
+    string key = argv[1];
+
+
+    for (int i=0; i<strlen(key); i++)
+    {
+        if(!isalpha(key[i]))
+        {
+            printf("The key is not alphabetical");
+            return 1;
+        }
+        else
+        key[i] = toupper(key[i]);
+
+    }
+    string text = GetString();
+
+    for (int i=0, k=0; i<strlen(text); i++)
+    {
+        if(isalpha(text[i]))
+        {
+            if (isupper(text[i]))
+        text[i] = (((text[i]-'A') + (key[(k++)%strlen(key)]-'A')) % 26) + 'A';
+
+                if(islower(text[i]))
+                text[i] = ((text[i]-'a') + (key[(k++)%strlen(key)]-'A')) % 26 + 'a';
+
+        }
+
+       else
+        text[i] = text[i];
+}
+printf("%s\n",text);
+
+return 0;
+}
